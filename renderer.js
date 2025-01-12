33d4cc5576d749122f317d72d03f7bbbd446caf6
Miguel Juarez <115324080+portablerug@users.noreply.github.com>
2025-01-12 09:30:49 -0800

    Delete renderer.js

--- renderer.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('start-scan').addEventListener('click', () => {
-    window.electron.send('start-scan', {});
-  });
-  
-  window.electron.receive('scan-result', (data) => {
-    console.log('Scan Result:', data);
-  });
-  
