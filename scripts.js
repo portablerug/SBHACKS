d8335b7a7bd8418fb5745bf3ae6bc1c656eee218
BeatrizAcostaa <131739604+BeatrizAcostaa@users.noreply.github.com>
2025-01-11 17:56:44 -0800

    Created JSON file for extension

--- scripts.js
+++ scripts.js
@@ -46,7 +46,21 @@ document.getElementById('config-form').addEventListener('submit', function(event
   
   function listenToChrome() {
     console.log('Listening to Google Chrome...');
+  
+    chrome.history.onVisited.addListener((historyItem) => {
+      console.log('Visited URL:', historyItem.url);
+      
+      // Check if the URL is a Google search
+      if (historyItem.url.startsWith('https://www.google.com/search')) {
+        const urlParams = new URLSearchParams((new URL(historyItem.url)).search);
+        const query = urlParams.get('q');
+        if (query) {
+          console.log('Search Query:', query);
+        }
+      }
+    });
   }
+  
   
   function listenToGmail() {
     console.log('Listening to Gmail...');
